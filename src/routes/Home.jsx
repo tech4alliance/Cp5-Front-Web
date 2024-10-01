3575d6c06d7f8f2de8fba96fd8b337970149de26
victor <victorcapp15@gmail.com>
2024-10-01 11:03:17 -0300

    estrutura um pouco mais avancada do home feita

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,7 +8,36 @@ const Home=()=>{
         <header>
             <h1>EletroDrive</h1>
         </header>
+
         <main>
+
+            <section className="bem-vindo">
+                <h1>Bem-vindo à EletroDrive, Á revolução da mobilidade elétrica!</h1>
+                <p>Na EletroDrive, estamos movendo o futuro. Somos apaixonados por inovação e sustentabilidade, oferecendo uma seleção  de veículos elétricos que combina tecnologia de ponta, eficiência e compromisso com o meio ambiente. Acreditamos que a mobilidade pode ser mais inteligente, mais ecológica e acessível para todos.</p>
+            </section>
+
+            <section className="razao">
+                <h2>Por que escolher a EletroDrive?</h2>
+                <nav>
+                    <ul>
+                        <li>Sustentabilidade em primeiro lugar: Todos os nossos veículos são 100% elétricos, proporcionando uma experiência de direção silenciosa e limpa, sem emissões de carbono.</li>
+                        <li>Tecnologia de ponta: A EletroDrive oferece os mais avançados modelos de carros elétricos, equipados com sistemas inteligentes de condução, design futurista e autonomia de alto desempenho.</li>
+                        <li>Economia e eficiência: Com custos reduzidos de manutenção e abastecimento, nossos carros oferecem uma solução econômica para quem busca inovar na maneira de se locomover.</li>
+                        <li>Atendimento especializado: Nossa equipe é composta por especialistas no setor automotivo elétrico, prontos para guiá-lo na escolha do veículo ideal para o seu estilo de vida.</li>
+                    </ul>
+                </nav>
+
+            <section className="missao">
+                <h2>Nossa missão</h2>
+                <p>Nossa missão é redefinir a experiência de dirigir, promovendo uma transformação para um mundo mais sustentável e inteligente. Na EletroDrive, a mobilidade do futuro já está ao seu alcance.</p>
+            </section>
+
+            <section className="modelos">
+                <h2>Nossos modelos</h2>
+                <p>Descubra nossa linha completa de veículos elétricos, desde compactos urbanos a SUVs de luxo, todos prontos para levar você a um novo patamar de inovação e conforto.</p>
+
+            </section>
+            </section>
             <article className="produtos-grid">
 
                 <section className="produto-item">
