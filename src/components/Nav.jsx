088fd51e87571e32a6ae40bbcd59b1e75b7b8df0
victor <victorcapp15@gmail.com>
2024-10-01 10:17:30 -0300

    Comecando a estruturar Login.jsx e comecando backend

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,10 @@ import {Link} from 'react-router-dom'
 const Nav=()=>{
     return(
         <>
-        
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/produtos">Produtos</Link>
+        <Link to="/sobre">Sobre</Link>
         </>
     )
 }
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,4 +1,23 @@
 const Login=()=>{
+
+    const handleSubmit=(e)=>{
+        //previne que sua pagina faça qualquer modificação ex. load
+        e.preventDefault();
+
+        if(validade()){
+
+            let token=
+                Math.random().toString(16).substring(2)+
+                Math.random().toString(16).substring(2)
+                sessionStorage.setItem("usuario",usuario.current.value);
+                sessionStorage.setItem("senha",token)
+                navigate("/cadastrarprodutos")
+
+        }else{
+                alert("usuario/senha inválidos")
+        }
+
+     }
     return(
         <>
         <h1>Login</h1>
