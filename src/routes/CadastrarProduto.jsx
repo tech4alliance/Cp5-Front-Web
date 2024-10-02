ef0f221efbff9b91e6b652e50820c5ca36a260bc
victor <victorcapp15@gmail.com>
2024-10-02 08:55:14 -0300

    Criando estrutura do CadastrarProdutos.jsx e Produtos.jsx

--- src/routes/CadastrarProduto.jsx
+++ src/routes/CadastrarProduto.jsx
@@ -2,7 +2,64 @@ const CadastrarProduto=()=>{
 
     return(
         <>
-        <h1>Produtos Cadastrados</h1>
+        <h1>Cadastrar Produtos</h1>
+        <table>
+            <tr>
+                <td>Carro</td>
+                <td>Preço</td>
+                <td>Fabricante</td>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+            <tr>
+                <td>BMW i8</td>
+                <td>2.000.00</td>
+                <td>BMW</td> <button>Cadastrar Carro</button>
+            </tr>
+        </table>
         </>
     )
 }
